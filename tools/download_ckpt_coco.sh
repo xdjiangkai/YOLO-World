30f0bb970501f2ce19b79eefe3bb0c7d71eb99af
Jiang Kai <36413328+xdjiangkai@users.noreply.github.com>
2026-04-07 13:15:26 +0800

    Fix download command for OpenAI CLIP model

--- tools/download_ckpt_coco.sh
+++ tools/download_ckpt_coco.sh
@@ -5,4 +5,4 @@ hf download wondervictor/YOLO-World-V2.1 --local-dir /hy-tmp/weights
 pip install modelscope
 modelscope download PAI/COCO2017 --local_dir /hy-tmp/data/
 
-hf download openai/clip-vit-base-patch32 --local-dir ../pretrained_models/openai/clip-vit-base-patch32
+hf download --dataset openai/clip-vit-base-patch32 --local-dir ../pretrained_models/openai/clip-vit-base-patch32
